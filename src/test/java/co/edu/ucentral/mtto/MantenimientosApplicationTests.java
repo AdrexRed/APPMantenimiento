211a0d468f1e870d98b568e3736c8c808814dbec
Andres Machuca <amachucar1@ucentral.edu.co>
2022-11-17 22:00:17 -0500

    generar archivo ejecutable

--- src/test/java/co/edu/ucentral/mtto/MantenimientosApplicationTests.java
+++ src/test/java/co/edu/ucentral/mtto/MantenimientosApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MantenimientosApplicationTests {
-
+// crado archivo ejecutable
 	@Test
 	void contextLoads() {
 	}
